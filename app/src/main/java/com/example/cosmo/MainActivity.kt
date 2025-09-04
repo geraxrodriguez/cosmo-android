cd94628e49db2f873dc39d3072ef4959fc5ccec6
gerardo rodriguez <gerarodriguez@proton.me>
2025-09-04 12:57:42 -0500

    Fix KSP version, organize build files, setup Moshi and Retrofit dependencies

--- app/src/main/java/com/example/cosmo/MainActivity.kt
+++ app/src/main/java/com/example/cosmo/MainActivity.kt
@@ -7,61 +7,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.cosmo.ui.AsteroidListScreen
 import com.example.cosmo.ui.theme.CosmoTheme
-import kotlinx.coroutines.*
-import com.example.cosmo.model.AsteroidRepository
-import android.util.Log
-import androidx.lifecycle.ViewModelProvider
-import com.example.cosmo.viewmodel.AsteroidViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var repository: AsteroidRepository // Hilt provides this
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val viewModel = ViewModelProvider(this)[AsteroidViewModel::class.java]
-        // Launch a coroutine to call fetchAsteroids
-        CoroutineScope(Dispatchers.IO).launch {
-            viewModel.fetchAsteroids("2024-01-01")
-        }
-
         setContent {
             CosmoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    innerPadding -> AsteroidListScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CosmoTheme {
-        Greeting("Android")
     }
 }
